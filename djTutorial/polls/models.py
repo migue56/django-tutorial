332536cafab936a11a2ac560221a0517411473ea
migue56 <miguel.arguedas.mejias>
2018-01-16 08:17:01 -0600

    Add Choice functions

--- djTutorial/polls/models.py
+++ djTutorial/polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from datetime import datetime
+
 
 # Create your models here.
 
@@ -16,11 +18,11 @@ class Question (models.Model):
 
 class Choice (models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
-    choise_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
         
     def __str__(self):
-        return self.choise_text
+        return self.choice_text
     
     
 
