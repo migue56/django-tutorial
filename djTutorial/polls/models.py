1ba38ecfe7ebf3f0155dd0c9203d728fc6ed494e
migue56 <miguel.arguedas.mejias>
2018-01-16 12:40:16 -0600

    Part7. Change admin GUI

--- djTutorial/polls/models.py
+++ djTutorial/polls/models.py
@@ -13,10 +13,13 @@ class Question (models.Model):
     def __str__(self):
         return self.question_text
         
-    def was_pusblished_recently(self):
+    def was_published_recently(self):
         now = timezone.now()
         return now - timedelta(days=1) <= self.pub_date <= now
         #return self.pub_date >= timezone.now() - timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'    
 
 class Choice (models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
